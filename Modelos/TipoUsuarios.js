3a41cf2dfd10f3711fe7f108543458f92ba36995
Spekter14 <khalicruz238@gmail.com>
2022-02-19 15:07:32 -0600

    Modelo Tipousuarios

--- Modelos/TipoUsuarios.js
+++ Modelos/TipoUsuarios.js
@@ -1,1 +1,24 @@
-//Tipo usuario
+const sequelize = require('sequelize');
+const db = require('../config/db');
+const TipoUsuario = db.define(
+    "tipousuarios",
+    {
+        id: {
+            type: sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+
+        tipo: {
+            type: sequelize.STRING(45),
+            allowNull: false,
+        },
+    },
+
+    {
+        tableName: "tipousuarios",
+        timestamps: false,
+    }
+);
+module.exports = TipoUsuario;
