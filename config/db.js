83ef9039f98fb93a96e37240a85ca5fc0998c01a
jhonatan182 <jona.vand98@gmail.com>
2022-02-19 15:44:43 -0600

    prueba

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('uber_app' ,'root', 'hola' , {
+const db = new Sequelize('uber_app' ,'root', 'jonamtv3d' , {
 
     host : '127.0.0.1',
     port : '3306',
