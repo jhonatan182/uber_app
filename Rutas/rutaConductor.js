c6f5e52f76d897bb473f85179d6567b55996351a
2pacbiggie25 <cristopherlanza@gmail.com>
2022-02-22 22:25:26 -0600

    RutaConductor

--- Rutas/rutaConductor.js
+++ Rutas/rutaConductor.js
@@ -3,6 +3,6 @@ const controladorConductor = require('../controladores/controladorConductor');
 const { body, query} = require('express-validator');
 const router = Router();
 
-
+router.post('/guardar', controladorVehiculos.editar);
 
 module.exports = router;
