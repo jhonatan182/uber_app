e19dd4e5b4289ede90b173583f33fe2cf4ae3761
Jhonatan Vargas <73511512+jhonatan182@users.noreply.github.com>
2022-02-24 00:28:24 -0600

    Revert "Rutas y Controladores Tipos Usuarios"

--- Rutas/rutasTipoUsuarios.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const controlTipU = require('../Controladores/controladorTipoUsuarios');
-const router = Router();
-
-router.get('/listartipu', controlTipU.ListarTipU);
-router.post('/guardartipu', controlTipU.GuardarTipU);
-router.put('/modificartipu', controlTipU.ActualizarTipoU);
-router.delete('/eliminartipu', controlTipU.EliminarTipoU);
