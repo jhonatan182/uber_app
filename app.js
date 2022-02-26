d148cdaacf0172599cbfe3fb05a36ac93f21fe03
Snga17 <suanygarcia174@gmail.com>
2022-02-26 15:19:33 -0600

    Corrección

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const db = require('./config/db');
 /* Rutas importacion*/
 
 const routerPrincipal = require('./Rutas/index');
-
+const routerUsuarios = require('./Rutas/rutasUsuarios')
 const app = express();
 
 /* comporobar la conexion a la base de datos */
 
 db.authenticate()
-    .then( () => console.log('Base de datos conectada') )
-    .catch(  error => console.log( error ));
+    .then(() => console.log('Base de datos conectada'))
+    .catch(error => console.log(error));
 
 /* morgan */
-app.use( morgan('dev') );
-app.use(express.urlencoded({extended:  false }));
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
 
 /* json */
 app.use(express.json());
@@ -25,12 +25,16 @@ app.set('json spaces', 2);
 
 
 /* rutas */
-app.use('/uber/api/' , routerPrincipal );
+app.use('/uber/api/', routerPrincipal);
+
+// Rutas de la api
+app.use('/uber/api/usuarios', routerUsuarios);
+
+
 
 /* configuracion del puerto */
 const port = process.env.PORT || 4000;
 
-app.listen(port , ()=> {
+app.listen(port, () => {
     console.log(`La aplicacion esta corriendo en el puerto ${port}`);
 });
-
