dbcb0438748872a6645538e490173c4ddc8a67a9
2pacbiggie25 <cristopherlanza@gmail.com>
2022-03-30 21:39:28 -0600

    Crud Ubicaciones

--- Controladores/controladorUbicaciones.js
+++ Controladores/controladorUbicaciones.js
@@ -21,7 +21,109 @@ const ubicaciones = async (req , res) => {
 
 };
 
+const guardarUbicacion = async (req , res) => {
+
+    const { nombre } = req.body;
+
+    if( !nombre ) {
+        res.json('Envie los datos necesarios');
+        return;
+    }
+
+    try {
+        
+        const guardarUbicacion = await Ubicaciones.create({
+           
+           nombre
+        });
+
+        if(guardarUbicacion) {
+            res.json('Datos guardados correctamente');
+        } else {
+            res.json('No se puedo guardar el registro');
+
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+const editarUbicacion = async (req, res) => {
+    const { id } = req.query;
+    const { nombre } = req.body;
+    if(!id || !nombre){
+        res.send("Datos Incompletos");
+    }
+    else{
+        var buscarubicaciones = await ubicaciones.findOne({
+            where:{
+                id: id,
+            }
+        });
+
+        if(!buscarubicaciones){
+            res.send("El id no existe");
+        }
+        else{
+            buscarubicaciones.nombre = nombre,
+            await buscarubicaciones.save()
+            .then((data) =>{
+                console.log(data);
+                res.send("Registro actualizado");
+            })
+            .catch((error) =>{
+                console.log(error);
+                res.send("Error al actualizar los datos");
+            });
+        }
+    }
+};
+
+const eliminarUbicacion = async (req, res) => {
+    const { id } = req.query;
+    if(!id){
+        res.send("Envie el id del registro");
+    }
+    else{
+        var buscarUbicacion = await Ubicaciones.findOne({
+            where:{
+                id: id,
+            }
+        });
+        if(!buscarUbicacion){
+            res.send("El id no existe");
+        }
+        else{
+            await Ubicaciones.destroy({
+                where:
+                {
+                    id: id,
+                }
+                
+            })
+            .then((data) => {
+                console.log(data);
+                res.send("Registro Eliminado");
+            })
+            .catch((error) => {
+                console.log(error);
+                res.send("Error al actualizar los datos");
+            });
+        } 
+
+    }
+};
+
+
+
 
 module.exports = {
-    ubicaciones
+    ubicaciones,
+    guardarUbicacion,
+    editarUbicacion,
+    eliminarUbicacion
+
 }
--- Rutas/rutaUbicaciones.js
+++ Rutas/rutaUbicaciones.js
@@ -1,10 +1,12 @@
-const { ubicaciones } = require('../Controladores/controladorUbicaciones');
+const { ubicaciones, guardarUbicacion,editarUbicacion,listarUbicacion,eliminarUbicacion} = require('../Controladores/controladorUbicaciones');
 const express = require('express');
-
 const router = express.Router();
 
 
 router.get('/', ubicaciones);
+router.post('/guardarUbicacion' , guardarUbicacion);
+router.put('/editarUbicacion', editarUbicacion);
+router.delete('/eliminarUbicacion', eliminarUbicacion);
 
 
 module.exports = router;
