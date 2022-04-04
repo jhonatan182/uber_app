18ceddfdb6f6a4d900d37977383e8367d4e5bd87
JessyLDA <hinadiaz15@gmail.com>
2022-04-04 01:13:38 -0600

    controlado tipo vehiculos actualizado
    se agregaron cosas a la api

--- Rutas/rutasTipoVehiculo.js
+++ Rutas/rutasTipoVehiculo.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { listarTiposVehiculos , guardarNuevoTipo, modificarTipoVehiculo, eliminarTipoVehiculo } = require('../Controladores/controladorTipoVehiculo');
+const { listarTiposVehiculos , obtenerId, guardarNuevoTipo, modificarTipoVehiculo, eliminarTipoVehiculo } = require('../Controladores/controladorTipoVehiculo');
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get('/listar', listarTiposVehiculos);
 router.post('/guardar', guardarNuevoTipo);
 router.put('/modificar', modificarTipoVehiculo);
 router.delete('/eliminar', eliminarTipoVehiculo);
+router.get('/obtenerid', obtenerId);
 
 module.exports = router;
